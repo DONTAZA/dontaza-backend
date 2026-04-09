952efb5284afb3058df3efea16548167fd652e47
PARK HYUNG GYUN <qhk1198@Gmail.com>
2026-04-09 19:49:09 +0900

    init: SpringBoot 빌드

---
+++ src/main/java/com/dontaza/dontazabackend/DontazaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dontaza.dontazabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontazaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DontazaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dontaza/dontazabackend/DontazaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontaza.dontazabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontazaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
