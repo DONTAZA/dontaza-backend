4a2f7ae7f1e78b7abb7c600e3d4567c174dcb372
PARK HYUNG GYUN <qhk1198@Gmail.com>
2026-04-11 21:53:51 +0900

    chore: 초기화 스크립트 작성

--- src/main/java/com/dontaza/dontazabackend/DontazaBackendApplication.java
+++ src/main/java/com/dontaza/dontazabackend/DontazaBackendApplication.java
@@ -2,11 +2,12 @@ package com.dontaza.dontazabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
 @EnableScheduling
